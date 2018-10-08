2e6f163b8546503461d49c8a98ea454dc6bf4d99
ThiagoQuaresma <thi_quaresma@hotmail.com>
2018-10-08 18:25:05 -0300

    Update Agenda/Consultas

--- src/app/layout/consulta/consulta.component.ts
+++ src/app/layout/consulta/consulta.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { routerTransition } from '../../router.animations';
 
 @Component({
   selector: 'app-consulta',
   templateUrl: './consulta.component.html',
-  styleUrls: ['./consulta.component.scss']
+  styleUrls: ['./consulta.component.scss'],
+  animations: [routerTransition()]
 })
 export class ConsultaComponent implements OnInit {
 
