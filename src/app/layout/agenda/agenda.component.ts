4a7b3d1a6cb48d15b983c3c8b4a29fa948c2af81
ThiagoQuaresma <ThiagoQuaresma>
2018-10-08 00:00:15 -0300

    Criação do modulo agendamento[1]

--- src/app/layout/agenda/agenda.component.ts
+++ src/app/layout/agenda/agenda.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { routerTransition } from '../../router.animations';
 
 @Component({
   selector: 'app-agenda',
   templateUrl: './agenda.component.html',
-  styleUrls: ['./agenda.component.scss']
+  styleUrls: ['./agenda.component.scss'],
+  animations: [routerTransition()]
 })
 export class AgendaComponent implements OnInit {
 
