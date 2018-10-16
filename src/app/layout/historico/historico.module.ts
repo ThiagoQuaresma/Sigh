f0f2546d45b2fce9e6585aa0417dd62901d1a1c0
ThiagoQuaresma <thi_quaresma@hotmail.com>
2018-10-16 20:44:54 -0300

    Atualização das telas

--- src/app/layout/historico/historico.module.ts
+++ src/app/layout/historico/historico.module.ts
@@ -4,11 +4,14 @@ import { CommonModule } from '@angular/common';
 import { HistoricoComponent } from './historico.component';
 import { HistoricoRoutingModule } from './historico-routing.module';
 
+import { TimelineComponent } from '../dashboard/components';
+import { DashboardComponent } from '../dashboard/dashboard.component';
+
 @NgModule({     
     imports: [CommonModule,
               HistoricoRoutingModule
             ],
-    declarations: [HistoricoComponent]
+    declarations: [HistoricoComponent,TimelineComponent,DashboardComponent]
 })
 
 export class HistoricoModule{
