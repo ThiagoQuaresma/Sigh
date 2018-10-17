dc8488a6a9b725f0a136b1887a12f912681de568
ThiagoQuaresma <ThiagoQuaresma>
2018-10-17 18:21:32 -0300

    Atualizando telas

--- src/app/layout/new-paciente/new-paciente.component.ts
+++ src/app/layout/new-paciente/new-paciente.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { routerTransition } from '../../router.animations';
 
 @Component({
   selector: 'app-new-paciente',
   templateUrl: './new-paciente.component.html',
-  styleUrls: ['./new-paciente.component.scss']
+  styleUrls: ['./new-paciente.component.scss'],
+  animations: [routerTransition()]
 })
 export class NewPacienteComponent implements OnInit {
 
