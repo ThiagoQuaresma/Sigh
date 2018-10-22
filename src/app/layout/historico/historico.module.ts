39c34a27c17d8c2d3eeb537aacd96790dc54a017
ThiagoQuaresma <thi_quaresma@hotmail.com>
2018-10-22 19:16:23 -0200

    Implementação do Historico

--- src/app/layout/historico/historico.module.ts
+++ src/app/layout/historico/historico.module.ts
@@ -4,14 +4,14 @@ import { CommonModule } from '@angular/common';
 import { HistoricoComponent } from './historico.component';
 import { HistoricoRoutingModule } from './historico-routing.module';
 
-import { TimelineComponent } from '../dashboard/components';
-import { DashboardComponent } from '../dashboard/dashboard.component';
+import { TimelineComponent } from './components';
+// import { DashboardComponent } from '../dashboard/dashboard.component';
 
 @NgModule({     
     imports: [CommonModule,
               HistoricoRoutingModule
             ],
-    declarations: [HistoricoComponent,TimelineComponent,DashboardComponent]
+    declarations: [HistoricoComponent, TimelineComponent]
 })
 
 export class HistoricoModule{
