f5c1bf0ea0189156b8e76352e60b31e5418430b4
ThiagoQuaresma <ThiagoQuaresma>
2018-11-26 16:09:23 -0200

    atualizando front

--- src/app/layout/consulta/consulta.module.ts
+++ src/app/layout/consulta/consulta.module.ts
@@ -1,8 +1,10 @@
+import { AgendaModule } from './../agenda/agenda.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ConsultaComponent } from './consulta.component';
 import { ConsultaRoutingModule } from './consulta-routing.module';
+
 
 @NgModule({     
     imports: [CommonModule,
